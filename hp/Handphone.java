0d007900d4a390f9a6e92b040e80550e8632bcfa
Bintang Asyrof <161684150+BintangAsyrof994@users.noreply.github.com>
2024-08-20 15:28:10 +0700

    Add files via upload

---
+++ hp/Handphone.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hp;
+
+/**
+ *
+ * @author Bintang
+ */
+public class Handphone {
+    public void hidupkan(){
+        System.out.println("Handphone Dihidupkan");
+    }
+    
+    public void LakukanPanggilan(){
+        System.out.println("Sedang melakukan panggilan");
+    }
+    
+    public void kirimSMS(){
+        System.out.println("Sedang mengirim SMS");
+    }
+    
+    public void matikan(){
+        System.out.println("Handphone dimatikan");
+    }
+    
+}
